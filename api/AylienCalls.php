bd0eceebc438217e7d2c1c695d969a53b9e904f2
jhinkel3 <jhinkel3@gmail.com>
2015-02-22 14:18:06 -0500

    api

---
+++ api/AylienCalls.php
@@ -0,0 +1,16 @@
+<?php
+# To use the SDK either use Composer's autoload
+#require __DIR__ . "/vendor/autoload.php";
+
+# Or manually
+ require_once("src/AYLIEN/TextAPI.php");
+
+$text = "John is a very good football player!";
+
+$textapi = new AYLIEN\TextAPI("b47f9237", "aea8e51bf28c8eb50f01c151590d9d69");
+$sentiment = $textapi->Sentiment(array("text" => $text));
+$language = $textapi->Language(array("text" => $text));
+
+echo json_encode($sentiment);
+echo json_encode($language);
+?>
