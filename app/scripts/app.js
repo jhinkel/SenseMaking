d322bf3c5d60cd2508816ee1d5dfb10e5a80d5bb
Andrew Nelson <andy@andyhub.com>
2015-02-22 14:07:37 -0500

    initialized file structure
    Install bower, run "bower install"

---
+++ app/scripts/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name senseMakingApp
+ * @description
+ * # senseMakingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('senseMakingApp', [
+    'ngAnimate',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
