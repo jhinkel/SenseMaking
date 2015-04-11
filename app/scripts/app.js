5f6a35b1b91805eb241f43fe154b97ffed1ebacd
Andrew Nelson <andy@andyhub.com>
2015-04-11 16:11:46 -0400

    Removed routing and ng-views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,17 +10,8 @@
 */
 angular.module('SenseMakingApp', [
     'ngAnimate',
-    'ngRoute',
     'SenseMakingApp.controllers',
     'SenseMakingApp.services'
 ])
-.config(function ($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-    })
-    .otherwise({
-        redirectTo: '/'
-    });
+.config(function () {
 });
