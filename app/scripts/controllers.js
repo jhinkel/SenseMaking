f4e56b5c1133277aa6d3b24236bdb264fdabb699
Andrew Nelson <andy@andyhub.com>
2015-04-11 16:44:29 -0400

    Started organizing month selection

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,7 +9,14 @@
 */
 angular.module('SenseMakingApp.controllers', [])
     .controller('MainCtrl', function ($scope, $log, API) {
+        $scope.months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
         API.getDocNumbers().then(function(response) {
             $scope.test = response[0];
         });
+
+        $scope.changeMonth = function(table) {
+            $scope.currentMonth = table;
+            console.log(table);
+        }
     });
