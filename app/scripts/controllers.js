6a0ae3d5e9c649f302eaefda9d4e61bac86fae4e
Andrew Nelson <andy@andyhub.com>
2015-04-13 22:22:00 -0400

    Cleaned up

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -13,95 +13,72 @@ angular.module('SenseMakingApp.controllers', [])
 
 
         $scope.setCurrentMonth = function (month) {
-        $scope.currentMonth = month;
-        $scope.keywords = {};
+            $scope.currentMonth = month;
+            $scope.keywords = {};
 
-        API.getDocumentsByMonth(month).then(function (documentIds) {
-				
-		function intersect(a, b) {
-            $scope.obituraries = [];
-
-            var t;
-            var intersect = [];
-            if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
-            return a.filter(function (e) {
-                if (b.indexOf(e) !== -1) return true;
-            });
-        }
-
-			API. getDocumentsByKeyword ("Obituaries").then(function (ObituraryIDs){
-				console.log(ObituraryIDs);
-				window.AllObituaries = ObituraryIDs;
-			})
-
-
-        intersect(AllObituaries, documentIds);
-        intersect = intersect(AllObituaries, documentIds);
-        console.log(	intersect);
-
-        angular.forEach(intersect, function(obiturary) {
-            API.getDocument(obiturary).then(
-                function(obiturary){
-                    console.log(obiturary);
-                    $scope.obituraries.push (obiturary);
+            console.log('fetching documents in ' + month);
+            API.getDocumentsByMonth(month).then(function (documentIds) {
+                console.log('fetched ' + documentIds.length + ' documents in ' + month);
+                function intersect(a, b) {
+                    var t;
+                    var intersect = [];
+                    if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+                    return a.filter(function (e) {
+                        if (b.indexOf(e) !== -1) return true;
+                    });
                 }
-            )
-
-
-        });//FindIntersection Ends
+                console.log('fetching documents containing \'Obituaries\'');
+                API.getDocumentsByKeyword("Obituaries").then(function (ObituraryIDs){
+                    console.log('fetched ' + ObituraryIDs.length + ' containing \'Obituaries\'');
+                    $scope.obituraries = [];
+                    intersect = intersect(ObituraryIDs, documentIds);
+                    angular.forEach(intersect, function(obiturary) {
+                        API.getDocument(obiturary).then(function(obiturary){
+                            console.log(obiturary);
+                            $scope.obituraries.push(obiturary);
+                        });
+                    });
+                });
 
                 //How the function below flows: find documentId --> find a specific keyword --> find sentiment & title/author
+                console.log('analyzing documents...');
                 angular.forEach(documentIds, function(documentId) {
-                    console.log(documentId);
-                    $scope.documentId = documentId;
-                    API.callAylien(documentId).then(function (response) {
-                        angular.extend($scope.keywords, response[0]['keyword']);
-                        //var keywords = response[0]['keyword'];
-                        //
-                        //angular.forEach(keywords, function(keyword) {
-                        //    API.getDocKeywordFrequency(documentId, keyword).then(
-                        //        $scope.SetDocContents = function SetDocContents(frequency){
-                        //            var polarity = "positive";
-                        //            var sentiment = Math.random();
-                        //            var r = Math.ceil (255 * sentiment);
-                        //            var g = Math.ceil (255 * sentiment);
-                        //            var b = Math.ceil (255 * sentiment);
-                        //            window.str = "AHS principal talks about course changes Story by: John Panni Date Published to Web: 4/1/2004"
-                        //            window.GetTitleIndex = str.indexOf("Story");
-                        //            window.GetAuthorIndex = str.indexOf("Date");
-                        //            window.GetPublishDate = str.indexOf("Web:");
-                        //            window.GetPublishDateStop = str.indexOf("2004");
-                        //            window.title = str.substring(0, GetTitleIndex);
-                        //            window.author = str.substring(GetAuthorIndex,GetTitleIndex);
-                        //            window.publishDate = str.substring(GetPublishDate + 4, GetPublishDateStop + 4);
-                        //            document.getElementById("title1").innerHTML = title;
-                        //            document.getElementById("AuthorAndPublishDate1").innerHTML = author + "|" + publishDate;
-                        //            if (polarity = "positive"){
-                        //                g += 150;
-                        //                console.log(sentiment);
-                        //                document.getElementById("sentiment").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-                        //            }//if positive
-                        //
-                        //            else {
-                        //                r += 150;
-                        //                console.log(polarity);
-                        //                document.getElementById("sentiment").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-                        //            }//else
-                        //        }//function SetSentiment
-                        //
-                        //
-                        //    ); //API.getDocKeywordFrequency
-                        //
-                        //});//for(keyword in keywords)
+                    API.callAylien(documentId).then(function(response) {
+                        var keywords = response[0]['keyword'];
+                        angular.extend($scope.keywords, keywords);
+                        angular.forEach(keywords, function(keyword) {
+                            API.getDocKeywordFrequency(documentId, keyword).then(function(frequency) {
+                                //$scope.SetDocContents = function(frequency) {
+                                //    var polarity = "positive";
+                                //    var sentiment = Math.random();
+                                //    var r = Math.ceil (255 * sentiment);
+                                //    var g = Math.ceil (255 * sentiment);
+                                //    var b = Math.ceil (255 * sentiment);
+                                //    window.str = "AHS principal talks about course changes Story by: John Panni Date Published to Web: 4/1/2004"
+                                //    window.GetTitleIndex = str.indexOf("Story");
+                                //    window.GetAuthorIndex = str.indexOf("Date");
+                                //    window.GetPublishDate = str.indexOf("Web:");
+                                //    window.GetPublishDateStop = str.indexOf("2004");
+                                //    window.title = str.substring(0, GetTitleIndex);
+                                //    window.author = str.substring(GetAuthorIndex,GetTitleIndex);
+                                //    window.publishDate = str.substring(GetPublishDate + 4, GetPublishDateStop + 4);
+                                //    document.getElementById("title1").innerHTML = title;
+                                //    document.getElementById("AuthorAndPublishDate1").innerHTML = author + "|" + publishDate;
+                                //    if (polarity = "positive"){
+                                //        g += 150;
+                                //        console.log(sentiment);
+                                //        document.getElementById("sentiment").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+                                //    } else {
+                                //        r += 150;
+                                //        console.log(polarity);
+                                //        document.getElementById("sentiment").style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+                                //    }//else
+                                //}
+                            });//API.getDocKeywordFrequency
+                        });//for(keyword in keywords)
                     });//API.callAylien(documentId), then
                 });//for
-
-
-
             });//API.getDocumentsByMonth
-
-
-
         }//setCurrentMonth
 
         $scope.setCurrentKeyword = function (keyword) {
@@ -121,7 +98,9 @@ angular.module('SenseMakingApp.controllers', [])
             });
         };
 
+        console.log('fetching document metadata');
         API.getDocDates().then(function (data) {
+            console.log('fetched document metadata');
             var DateArray = [];
             var MonthArray = [];
             var MonthXAxis = $scope.months;
