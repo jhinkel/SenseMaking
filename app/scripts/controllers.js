05dfb7e14b4aa4ae1909da0b8bac8b96c740a1ae
Andrew Nelson <andy@andyhub.com>
2015-04-14 21:48:55 -0400

    Forgot to remove this

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -109,10 +109,6 @@ angular.module('SenseMakingApp.controllers', [])
             });
         };
 
-        $scope.filterKeywords = function() {
-            $scope.keywords;
-        };
-
         console.log('fetching document metadata');
         API.getDocDates().then(function (data) {
             console.log('fetched document metadata');
